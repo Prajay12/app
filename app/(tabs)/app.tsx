7a161c21d005d631c669047bcef2bac3a419e0dd
Prajay12 <149463273+Prajay12@users.noreply.github.com>
2025-01-13 10:32:56 +0000

    updates

--- app/(tabs)/app.tsx
+++ app/(tabs)/app.tsx
@@ -1,12 +1,15 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity } from "react-native";
 
-const app = () => {
+export default function App() {
   return (
-    <View>
-      <Text className='text-white'>app</Text>
+    <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
+      <TouchableOpacity
+        onPress={() => console.log("Button pressed")}
+        disabled={true} // Disables the touchable
+        className="bg-white"
+      >
+        <Text style={{ color: "black", fontSize: 16 }}>Press</Text>
+      </TouchableOpacity>
     </View>
-  )
+  );
 }
-
-export default app
